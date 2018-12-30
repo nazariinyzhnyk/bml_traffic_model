df0c031bb2090fc17283e3bbf6fe5df747efa061
nazariinyzhnyk <nazar.nyzhnyk@gmail.com>
2018-12-30 17:15:56 +0200

    emerging logic added

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ parser.add_argument("-frows", "--fieldrows", help="field size rows", default='9'
 parser.add_argument("-b", "--blues", help="blue cars ratio", default='0.3333')  # todo: add param to pass only density
 parser.add_argument("-r", "--reds",  help="red cars ratio", default='0.3333')
 parser.add_argument("-i", "--iterations",  help="num of iterations", default='2')
-parser.add_argument("-e", "--emergence",  help="new elements arising param", default='same')
 parser.add_argument("-s", "--sleep",  help="sleep time", default='0.1')
 args = parser.parse_args()
 
@@ -50,7 +49,7 @@ if __name__ == '__main__':  # todo: add comments
     prev_iter_field = field.copy()
     for i in range(int(args.iterations)):
         title_iter_num = str(i + 1)
-        field = car_moves(field, emerge=args.emergence)
+        field = car_moves(field)
         if np.array_equal(np.array(field), np.array(prev_iter_field)):
             print('System stucked after ' + title_iter_num + ' iterations.')
             plot_matrix(field, 'Final iteration = ' + title_iter_num, pause_time=5.0)
--- moves.py
+++ moves.py
@@ -1,35 +1,76 @@
-def move_right(field, emergence='none', move_value=2):  # todo: add comments
-    rownum = len(field)
-    colnum = len(field[0])
-    for i in range(rownum):
-        for j in range(colnum - 2, -1, -1):
-            if field[i][j] == move_value and field[i][j + 1] == 0:
-                field[i][j + 1] = field[i][j]
-                field[i][j] = 0
+
+
+# def move_right(field, move_value=2):  # todo: add comments
+#     rownum = len(field)
+#     colnum = len(field[0])
+#     for i in range(rownum):
+#         for j in range(colnum - 2, -1, -1):
+#             if field[i][j] == move_value and field[i][j + 1] == 0:
+#                 field[i][j + 1] = field[i][j]
+#                 field[i][j] = 0
+#     return field
+#
+#
+# def move_down(field, move_value=1):  # todo: add comments, write emergence logic
+#     rownum = len(field)
+#     colnum = len(field[0])
+#     start_row = 2
+#     idx_replacable = [el for el, x in enumerate(field[0]) if x == 0]
+#     idx_to_replace = [el for el, x in enumerate(field[rownum - 1]) if x == 1]
+#     replacable = list(set(idx_to_replace).intersection(set(idx_replacable)))  # fixme: circular filling
+#     field[0] = [el if x not in replacable else 1 for x, el in enumerate(field[0])]  # check me
+#     field[rownum - 1] = [el if x not in replacable else 0 for x, el in enumerate(field[rownum-1])]
+#     for i in range(rownum - 2, -1, -1):
+#         for j in range(colnum):
+#             if field[i][j] == move_value and field[i + 1][j] == 0:
+#                 field[i + 1][j] = field[i][j]
+#                 field[i][j] = 0
+#     return field
+
+def move_loop(arr, range_from, range_to, move_value=2):
+    for i in range(range_from, range_to, -1):
+        if arr[i] == move_value and arr[i + 1] == 0:
+            arr[i + 1] = arr[i]
+            arr[i] = 0
+    return arr
+
+
+def circular_filling(vect, move_value=2):
+    vect_len = len(vect)
+    if vect[vect_len - 1] == move_value:
+        if vect[0] == 0:
+            vect[0] = move_value
+            vect[vect_len - 1] = 0
+            vect = move_loop(vect, vect_len - 2, 0, move_value=move_value)
+        else:
+            temp_vect = vect.copy()
+            temp_vect[vect_len - 1] = 0
+            temp_vect = move_loop(temp_vect, vect_len - 2, -1, move_value=move_value)
+            if temp_vect[0] == 0:
+                temp_vect[0] = move_value
+                vect = temp_vect.copy()
+            else:
+                vect = move_loop(vect, vect_len - 2, -1, move_value=move_value)
+    else:
+        vect = move_loop(vect, vect_len - 2, -1, move_value=move_value)
+    return vect
+
+
+def move_right(field, move_value=2):
+    for i in range(len(field)):
+        field[i] = circular_filling(field[i], move_value=move_value)
     return field
 
 
-def move_down(field, emergence='none', move_value=1):  # todo: add comments, write emergence logic
-    rownum = len(field)
-    colnum = len(field[0])
-    start_row = 2
-    if emergence != 'none':
-        idx_replacable = [el for el, x in enumerate(field[0]) if x == 0]
-        idx_to_replace = [el for el, x in enumerate(field[rownum - 1]) if x == 1]
-        replacable = list(set(idx_to_replace).intersection(set(idx_replacable)))  # fixme: circular filling
-        field[0] = [el if x not in replacable else 1 for x, el in enumerate(field[0])]  # check me
-        field[rownum - 1] = [el if x not in replacable else 0 for x, el in enumerate(field[rownum-1])]
-    for i in range(rownum - 2, -1, -1):
-        for j in range(colnum):
-            if field[i][j] == move_value and field[i + 1][j] == 0:
-                field[i + 1][j] = field[i][j]
-                field[i][j] = 0
+def move_down(field, move_value=1):
+    for i in range(len(field[0])):
+        field[:, i] = circular_filling(field[:, i], move_value=move_value)
     return field
 
 
-def car_moves(field, emerge='none', move_right_value=2, move_down_value=1):  # todo: add comments
-    field = move_right(field, emergence=emerge, move_value=move_right_value)
-    field = move_down(field, emergence=emerge, move_value=move_down_value)
+def car_moves(field, move_right_value=2, move_down_value=1):  # todo: add comments
+    field = move_right(field, move_value=move_right_value)
+    field = move_down(field, move_value=move_down_value)
     return field
 
 # todo: add move_up, move_left
