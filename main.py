8223940d6debfd99ff94008860949eb7ed259b15
nazariinyzhnyk <nazar.nyzhnyk@gmail.com>
2019-01-05 19:15:05 +0200

    argparser fixes

--- main.py
+++ main.py
@@ -7,23 +7,24 @@ from moves import car_moves, get_field, plot_matrix
 warnings.filterwarnings("ignore")
 cmap = ListedColormap(['white', '#95d0fc', '#ff474c'])
 
-parser = argparse.ArgumentParser(description='Initial conditions: ')
-parser.add_argument("-fcols", "--fieldcols", help="field size cols", default='9')
-parser.add_argument("-frows", "--fieldrows", help="field size rows", default='9')
-parser.add_argument("-d", "--density", help="density", default='0')
-parser.add_argument("-b", "--blues", help="blue cars ratio", default='0.3333')
-parser.add_argument("-r", "--reds",  help="red cars ratio", default='0.3333')
-parser.add_argument("-i", "--iterations",  help="num of iterations", default='2')
-parser.add_argument("-s", "--sleep",  help="sleep time", default='0.1')
-parser.add_argument("-p", "--plot",  help="save plot", default='0')
-args = parser.parse_args()
-
-if float(args.density) != 0:  # able to pass only density arg
-    args.blues = '0.' + args.density
-    args.reds = '0.' + args.density
-
 
 if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Initial conditions: ')
+    parser.add_argument("-fcols", "--fieldcols", help="field size cols", default='9')
+    parser.add_argument("-frows", "--fieldrows", help="field size rows", default='9')
+    parser.add_argument("-d", "--density", help="density", default='0')
+    parser.add_argument("-b", "--blues", help="blue cars ratio", default='0.3333')
+    parser.add_argument("-r", "--reds", help="red cars ratio", default='0.3333')
+    parser.add_argument("-i", "--iterations", help="num of iterations", default='2')
+    parser.add_argument("-s", "--sleep", help="sleep time", default='0.1')
+    parser.add_argument("-p", "--plot", help="save plot", default='0')
+    args = parser.parse_args()
+
+    if float(args.density) != 0:  # able to pass only density arg
+        args.blues = '0.' + args.density
+        args.reds = '0.' + args.density
+
     plt.ion()
     plt.figure()
     field = get_field(int(args.fieldcols), int(args.fieldrows), [float(args.blues), float(args.reds)])
